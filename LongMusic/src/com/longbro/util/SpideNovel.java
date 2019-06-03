744200bcc896aba1c68d6f8738a964c11b442b5e
ZhaoChengong <1459892910@qq.com>
2019-06-03 09:18:46 +0800

    一并输出拼接及下载所用的时间

--- LongMusic/src/com/longbro/util/SpideNovel.java
+++ LongMusic/src/com/longbro/util/SpideNovel.java
@@ -3,8 +3,11 @@ package com.longbro.util;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 import org.jsoup.Jsoup;
@@ -15,7 +18,7 @@ public class SpideNovel {
 	public static ArrayList<HashMap<String, String>> spideList() throws IOException{
 		ArrayList<HashMap<String, String>> list=new ArrayList<HashMap<String, String>>();
 		
-		Document doc=Jsoup.connect("https://www.biqudao.com/bqge228154/").get();
+		Document doc=Jsoup.connect("https://www.biqudao.com/bqge162771/").get();
 		Element e=doc.getElementById("list");
 		String chapterList=e.toString();
 		String d[]=chapterList.split("<dt>");
@@ -47,7 +50,7 @@ public class SpideNovel {
 	 * @throws IOException
 	 */
 	public static String spideContent(String page,String name) throws IOException{
-		Document doc=Jsoup.connect("https://www.biqudao.com/bqge228154/"+page+".html").get();
+		Document doc=Jsoup.connect("https://www.biqudao.com/bqge162771/"+page+".html").get();
 		Element e=doc.getElementById("content");
 		String chapterCon=e.toString().replaceAll("<div id=\"content\">", "")
 				.replaceAll("<br>", "")
@@ -56,19 +59,32 @@ public class SpideNovel {
 		return chapterCon;
 	}
 	public static void saveNovel() throws IOException{
-		String content="";
-		File file=new File("E:/AAAA/novel/novel1.txt");
+		long begin=System.currentTimeMillis();//开始下载时的时间
+		String content=new String();
+		String suse="";//使用的秒数
+		long use=0;
+		File file=new File("D:/long/novel2.txt");
 		file.createNewFile();
 		ArrayList<HashMap<String, String>> list=spideList();
 		for(HashMap<String, String> map:list){
 			String chapterCon=spideContent(map.get("link"),map.get("title"));
 			content=content+map.get("title")+chapterCon;
-			
-			System.out.println("\""+map.get("title")+"\"拼接成功");
+			//new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date())
+			long now=System.currentTimeMillis();//拼接本章时的时间
+			use=now-begin;//使用的毫秒数
+			suse=use/1000+"";//使用的秒数
+			System.out.println("\""+map.get("title")+"\"拼接成功，已使用"+use+"毫秒，"+suse+"s");
 		}
 //		System.out.println(content);
-		FileOutputStream fos=new FileOutputStream(file);
-		fos.write(content.getBytes());
+		//使用字节流
+//		FileOutputStream fos=new FileOutputStream(file);
+//		fos.write(content.getBytes());
+		//使用字符流
+		FileWriter out=new FileWriter(file);
+		out.write(content);
+		System.out.println("下载小说成功，共使用"+suse+"s");
+
+		out.close();
 	}
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
