cac3e4b238a30bf59485c5a54bca2aaa02567a95
ZhaoChengong <1459892910@qq.com>
2019-09-24 13:47:26 +0800

    ,,

--- LongMusic/src/com/longbro/util/SpideMusic.java
+++ LongMusic/src/com/longbro/util/SpideMusic.java
@@ -1,5 +1,4 @@
 package com.longbro.util;
-
 import java.io.IOException;
 import java.util.HashMap;
 
@@ -12,7 +11,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 /**
- * 网易云音乐新歌榜、热歌榜歌曲爬取
+ * 网易云音乐新歌榜、热歌榜歌曲爬取~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~已被网易云音乐废掉！
  * 使用JackSon解析json数据，取得其中的歌曲信息
  * -https://blog.csdn.net/taiyangdao/article/details/80668416
  * @author 赵成龙
@@ -24,15 +23,29 @@ import org.jsoup.select.Elements;
 public class SpideMusic {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try {
+//		for(int i=0;i<100;i++){
+			ttt();
+//		}
+	}
+
+	private static void ttt() {
+		try {//ftype:23,status:42,transNames:7,type:32,publishTime:5,privilege:12
 			//https://music.163.com/#/discover/toplist?id=3779629新歌榜
 			//https://music.163.com/#/discover/toplist?id=19723756飙升榜
-			Document doc=Jsoup.connect("https://music.163.com/discover/toplist?id=3779629").get();
+			Document doc=Jsoup.connect("https://music.163.com/m/discover/toplist?id=19723756").get();
 			Elements es=doc.getElementsByTag("textarea");
 			String songs=es.get(0).toString();
 			songs=songs.replaceAll("<[.[^<]]*>", "");//<[.[^<]]*>
 //			songs=songs.substring(songs.indexOf("display:none")+15, songs.indexOf("</textarea>"));
-			byFactory(songs);
+//			System.out.println(songs);
+			String swhat=songs.substring(1, 18);
+			System.out.println(swhat);
+//			String []songsL=songs.split("\\},\\{");
+//			for(int i=0;i<songsL.length;i++){
+//				System.out.println(songsL[i]);
+//			}
+
+//			byFactory(songs);
 			//使用ObjectMapper
 //			ObjectMapper mapper = new ObjectMapper();
 //			SongInfo infoMap = mapper.readValue(songs, SongInfo.class);
@@ -54,7 +67,7 @@ public class SpideMusic {
 		while(jp.nextToken() != JsonToken.END_OBJECT){
 			String fieldName = jp.getCurrentName();
 				System.out.println(fieldName);
-			if ("duration".equals(fieldName)){
+			if ("name".equals(fieldName)){
 				System.out.println(jp.getText());//jp.getText()
 			}
 			jp.nextToken();
