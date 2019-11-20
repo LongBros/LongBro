0ecee600f139675075c4a714c39273616240b965
LongBros <1459892910@qq.com>
2019-11-20 22:37:52 +0800

    上线关注功能

--- LongMusic/src/com/longbro/note/controller/AttentionController.java
+++ LongMusic/src/com/longbro/note/controller/AttentionController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.longbro.house.bean.BaseResult;
 import com.longbro.note.bean.Attention;
 import com.longbro.note.service.AttentionService;
 import com.longbro.util.TimeUtil;
@@ -35,9 +36,60 @@ public class AttentionController{
      */
     @RequestMapping(value="noticeAuthor",method=RequestMethod.GET)
     @ResponseBody
-    public void noticeAuthor(Attention att){
+    public BaseResult noticeAuthor(Attention att){
+    	BaseResult bs=new BaseResult<>();
+    	if(StringUtils.isEmpty(att.getNNoticer())){
+    		bs.setCode(110);
+    		bs.setMessage("请先登录");
+    		return bs;
+    	}
     	att.setNNoticeTime(TimeUtil.time());
     	att.setNReadStatus(0);
     	attentionService.create(att);
+    	bs.setCode(200);
+    	bs.setMessage("关注成功");
+    	return bs;
+    }
+    /**
+     * @desc 判断当前登录用户是否已关注某作者
+     * @author zcl
+     * @date 2019年11月20日
+     */
+    @RequestMapping(value="whetherHasNotice",method=RequestMethod.GET)
+    @ResponseBody
+    public BaseResult whetherNotice(Attention att){
+    	BaseResult bs=new BaseResult<>();
+    	Attention atten=attentionService.whetherNotice(att);
+    	//当前人未登录或登录未关注，显示“关注”按钮
+    	if(atten==null||StringUtils.isEmpty(att.getNNoticer())){
+    		bs.setCode(110);
+    		bs.setMessage("当前人未登录或登录未关注");
+    		return bs;
+    	}
+    	bs.setCode(200);
+		bs.setMessage("当前人已关注当前作者");
+		return bs;
+    }
+    /**
+     * @desc 取消关注
+     * @author zcl
+     * @date 2019年11月20日
+     * @param att
+     * @return
+     */
+    @RequestMapping(value="cancelAtten",method=RequestMethod.GET)
+    @ResponseBody
+    public BaseResult cancelAtten(Attention att){
+    	BaseResult bs=new BaseResult<>();
+    	if(StringUtils.isEmpty(att.getNNoticer())){
+    		bs.setCode(110);
+    		bs.setMessage("请先登录");
+    		return bs;
+    	}
+    	
+    	attentionService.cancelAtten(att);
+    	bs.setCode(200);
+    	bs.setMessage("取消关注成功");
+    	return bs;
     }
 }
