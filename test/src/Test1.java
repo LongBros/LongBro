070648d9683dc68bd4a5736eb54ec55d7bc65aaf
ZhaoChengong <1459892910@qq.com>
2019-12-06 18:07:21 +0800

    总结文件的修改

--- test/src/Test1.java
+++ test/src/Test1.java
@@ -11,12 +11,17 @@ import java.util.List;
  */
 public class Test1 {
 	public static void main(String[] args) throws UnsupportedEncodingException {
-		test1();
-
-		System.out.println(new String("天河区".getBytes(),"UTF-8"));
-		test();
-		String s=URLDecoder.decode("%E7%AE%A1%E7%90%86%E5%91%98");
-		System.out.println(s);
+		for(int i=1;i<34;i++){
+			System.out.print("@a"+i+",");
+		}
+		
+		
+//		test1();
+//
+//		System.out.println(new String("天河区".getBytes(),"UTF-8"));
+//		test();
+//		String s=URLDecoder.decode("%E7%AE%A1%E7%90%86%E5%91%98");
+//		System.out.println(s);
 	}
 	/**
 	 * 测试string的split、list的remove
